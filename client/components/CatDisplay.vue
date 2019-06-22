099c438bd1a95228483b6d991ed12d455443fd7c
llabati <labati.ludovic@orange.fr>
2019-06-22 12:14:20 +0200

    Set up the store and the db

---
+++ client/components/CatDisplay.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        <img class="img-fluid card-img-top" src="static/img/PELOTE.jpg">
+        <caption class="text-center">PELOTE</caption>
+        
+        <!-- <cat-vote :currentCat="random[0]" v-on:vote="transmitVote(side)"></cat-vote>  -->
+        
+    </div>  
+
+</template>
+
+<script>
+import { store } from '../store'
+import CatVote from './CatVote'
+
+export default {
+    name: 'CatDisplay',
+    store,
+    props: {
+        cat: Object
+    },
+    /*data(){
+        return{
+            side: left
+        }
+    },
+    methods: {
+        transmitVote(side){
+            return $emit('support', this.side)
+        }
+    },
+    components: {
+        CatVote
+    }  */
+
+}
+</script>
