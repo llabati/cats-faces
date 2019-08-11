02f6045a63549ea0817d98049e9888bd6c082bc2
llabati <33926231+llabati@users.noreply.github.com>
2019-08-11 08:52:26 +0200

    Adding users

--- client/router.js
+++ client/router.js
@@ -4,11 +4,13 @@ import VueRouter from 'vue-router'
 import VotingPage from './components/VotingPage.vue'
 import CatPage from './components/CatPage.vue'
 import RankingPage from './components/RankingPage.vue'
+import ConnectionPage from './components/ConnectionPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/', name:'voting', component: VotingPage },
+    { path: '/', name: 'connection', component: ConnectionPage },
+    { path: '/vote', name:'voting', component: VotingPage },
     { path: '/rank', name: 'ranking', component: RankingPage },
     { path: '/cat/:name', name: 'cat', component: CatPage }
 ]
