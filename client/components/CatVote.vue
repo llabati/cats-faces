46f94f5c3a02d091b273bfc9f640738f6ff24e97
llabati <33926231+llabati@users.noreply.github.com>
2019-08-11 08:48:27 +0200

    Adding users

--- client/components/CatVote.vue
+++ client/components/CatVote.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="container mx-auto">
-            <button id="btn-vote" class="btn btn-outline my-2 float-right lead text-muted" @click="$emit('vote')">Votez pour {{ cat.name }}</button>
+            <button id="btn-vote" class="btn btn-outline my-2 float-right lead text-muted" @click="$emit('vote', this.cat)">Votez pour {{ cat.name }}</button>
         </div>
     </div>
 </template>
@@ -20,6 +20,9 @@ export default {
         border-color: red;
     }
 </style>
+
+
+
 
 
 
