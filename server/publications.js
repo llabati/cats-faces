d4b5aa04e0cf8e589c89fec9b60daf0cc1a28849
llabati <33926231+llabati@users.noreply.github.com>
2019-08-11 08:57:05 +0200

    Adding users

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,22 @@
 import { Meteor } from 'meteor/meteor'
-import { Cats } from '../lib/collections'
+import { Cats, Humans, Comments } from '../lib/collections'
 
 Meteor.publish('cats', function(){
     return Cats.find({})
 })
+/*
+Meteor.publish('thiscat', function(name){
+    return Cats.find({name: name})
+})
+*/
+Meteor.publish('humans', function(){
+    return Humans.find({})
+})
 
+Meteor.publish('comments', function(){
+    return Comments.find({})
+})
+
+Meteor.publish('len', function(){
+    return Cats.find({}).count()
+})
