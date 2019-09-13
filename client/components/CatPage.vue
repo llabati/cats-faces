1ac9498f1dd927126ba5b79e0f6addc1606bf522
llabati <33926231+llabati@users.noreply.github.com>
2019-09-13 17:36:51 +0200

    Enhancing scoring

--- client/components/CatPage.vue
+++ client/components/CatPage.vue
@@ -1,24 +1,16 @@
 <template>
     <div class="card">
-
-        <section>
-            <h1 class="display-4 text-center text-info">{{ name }}</h1>
-            <div class="col-6 offset-3">
-                <img :src="`public/${cat.pic}`" :alt="cat.name"/>
-                
-            </div>
-        </section>
         <section class="card-body">
-            <h2 id="com" class="display-5 text-center text-muted">Tous les commentaires sur ce chat</h2>
+            <h2 id="com" class="display-5 text-center text-muted">Tous les commentaires sur <span class="text-info">{{ name }}</span></h2>
             <ul class="list-group">
-                <li v-for="comment in comments" :key="comment.id" class="list-group-item bg-gray">
-                    <p><strong>{{ comment.author }}</strong></p>
-                    <p class="bg-white">{{ comment.content }}</p>
+                <li v-for="comment in comments" :key="comment.id" class="list-group-item bg-secondary">
+                    <p class="text-white"><strong>{{ comment.author }}</strong><em style="margin-left: 15px;">Posté le {{ comment.date }} </em></p>
+                    <p class="bg-white" style="padding: 2%">{{ comment.content }}</p>
                 </li>
             </ul>
         </section>
         <div class="card-footer">
-                <button class="btn btn-info float-right" @click="$router.push('/vote')">Continuer à voter</button>
+                <button class="btn btn-info float-right" @click="returnToPreviousPage">Continuer à voter</button>
         </div>
     </div>
 </template>
@@ -52,10 +44,18 @@ export default {
 
     computed: {
         cat(name) {
-            console.log(this.cats.find(c => c.name === this.name))
+            console.log('THIS CAT COMPUTED', this.cats.find(c => c.name === this.name))
             return this.cats.find(c => c.name === this.name)
         },
     },
+    methods: {
+        returnToPreviousPage(){
+            history.back()
+        }
+    },
+    mounted(){
+        console.log('THIS CAT', this.cat)
+    }
     
     
 }
